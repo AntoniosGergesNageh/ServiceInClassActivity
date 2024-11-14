6f8045e231aa0aa69020afe8361de8ff2b7d8acf
t00nny <129798251+t00nny@users.noreply.github.com>
2024-11-14 14:58:29 -0500

    Implement button functionality.

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -1,21 +1,37 @@
 package edu.temple.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var timerTextView: TextView
+    var timerBinder: TimerService.TimerBinder? = null
+    var isconnected = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        timerTextView = findViewById(R.id.textView)
+        val button = findViewById<Button>(R.id.startButton)
+        button.setOnClickListener {
+            if (isconnected && button.text == "Start") {
+                timerBinder?.start(100)
+                button.text = "Pause"
 
-        findViewById<Button>(R.id.startButton).setOnClickListener {
-
+            } else if (isconnected && button.text == "Pause") {
+                timerBinder?.pause()
+                button.text = "Start"
+            }
         }
-        
-        findViewById<Button>(R.id.stopButton).setOnClickListener {
 
+        findViewById<Button>(R.id.stopButton).setOnClickListener {
+            timerBinder?.stop()
         }
     }
+
+
 }
